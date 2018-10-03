1931023895cd0d6998e01c4294514a88c51d5daa
holymoly <holzmolz@googlemail.com>
2018-10-03 12:12:52 +0200

    Create gen_certs.sh

---
+++ gen_certs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir /var/db/mysql/ssl
+
+#Generate ca rsa key
+openssl genrsa 2048 > /var/db/mysql/ssl/ca-key.pem
+
+#Generate ca cert
+openssl req -new -x509 -nodes -days 365000 -key /var/db/mysql/ssl/ca-key.pem -out /var/db/mysql/ssl/ca-cert.pem
+
+#Generate server rsa key
+openssl req -newkey rsa:2048 -days 365000 -nodes -keyout /var/db/mysql/ssl/server-key.pem -out /var/db/mysql/ssl/server-req.pem
+
+#Export the server RSA Public Key to a File
+openssl rsa -in /var/db/mysql/ssl/server-key.pem -out /var/db/mysql/ssl/server-key.pem
+
+#Sign server certificate
+openssl x509 -req -in /var/db/mysql/ssl/server-req.pem -days 365000 -CA /var/db/mysql/ssl/ca-cert.pem -CAkey /var/db/mysql/ssl/ca-key.pem -set_serial 01 -out /var/db/mysql/ssl/server-cert.pem
+
+#Generate clent rsa key
+openssl req -newkey rsa:2048 -days 365000 -nodes -keyout /var/db/mysql/ssl/client-key.pem -out /var/db/mysql/ssl/client-req.pem
+
+#Export the client RSA Public Key to a File
+openssl rsa -in /var/db/mysql/ssl/client-key.pem -out /var/db/mysql/ssl/client-key.pem
+
+#Sign client certificate
+openssl x509 -req -in /var/db/mysql/ssl/client-req.pem -days 365000 -CA /var/db/mysql/ssl/ca-cert.pem -CAkey /var/db/mysql/ssl/ca-key.pem -set_serial 01 -out /var/db/mysql/ssl/client-cert.pem
+
+#Verify certificates
+openssl verify -CAfile var/db/mysql/ssl/ca-cert.pem var/db/mysql/ssl/server-cert.pem var/db/mysql/ssl/client-cert.pem
