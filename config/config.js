a29abcd6bf853dcde81e26ff13ba9163c4690ebb
unknown <holzmolz@googlemail.com>
2022-02-22 16:07:03 +0100

    updated pg

--- config/config.js
+++ config/config.js
@@ -5,52 +5,52 @@ var fs = require('fs');
 
 // Server Configuration
 const hapiServer = {
-  port: process.env.HAPIPORT || 8000,
-  address: process.env.HAPIHOST || '127.0.0.1',
-  //Uncomment for https
-  //tls: {
-  //  key: process.env.HAPIKEY || fs.readFileSync('./config/certs/server-key.pem'),
-  //  cert: process.env.HAPICERT || fs.readFileSync('./config/certs/server-cert.pem')
-  //}
+    port: process.env.HAPIPORT || 8000,
+    address: process.env.HAPIHOST || '127.0.0.1',
+    //Uncomment for https
+    //tls: {
+    //  key: process.env.HAPIKEY || fs.readFileSync('./config/certs/server-key.pem'),
+    //  cert: process.env.HAPICERT || fs.readFileSync('./config/certs/server-cert.pem')
+    //}
 }
 
 // postgres configuration
 const postgres = {
-  host: process.env.PGHOST || 'localhost',
-  port: process.env.PGPORT || '5432',
-  user: process.env.PGUSER || 'root',
-  password: process.env.PGPASSWORD || 'test',
-  database: process.env.PGDATABASE || 'api',
+    host: process.env.PGHOST || '10.42.23.10',
+    port: process.env.PGPORT || '5432',
+    user: process.env.PGUSER || 'postgres',
+    password: process.env.PGPASSWORD || 'postgres',
+    database: process.env.PGDATABASE || 'postgres',
 }
 
 // rabbitmq databus
 const databus = {
-  host: process.env.DATABUSHOST || '127.0.0.1',
-  port: process.env.DATABUSPORT || 1883,
-  username: process.env.DATABUSUSER || 'test',
-  password: process.env.DATABUSPASSWORD || 'test',
-  //key: process.env.DATABUSSSLKEY || fs.readFileSync('./config/certs/client-key.pem'),
-  //cert: process.env.DATABUSSSLCERT || fs.readFileSync('./config/certs/client-cert.pem'),
-  //rejectUnauthorized: process.env.DATABUSSSLREJECTUNAUTHORIZED || false,
-  //##### The CA list will be used to determine if server is authorized
-  //ca: process.env.DATABUSSSLCA || fs.readFileSync('./config/certs/ca-cert.pem'),
-  protocol: process.env.DATABUSPROTOCOL || 'mqtt' // mqtt or mqtts
+    host: process.env.DATABUSHOST || '10.42.23.11',
+    port: process.env.DATABUSPORT || 1883,
+    username: process.env.DATABUSUSER || 'test',
+    password: process.env.DATABUSPASSWORD || 'test',
+    //key: process.env.DATABUSSSLKEY || fs.readFileSync('./config/certs/client-key.pem'),
+    //cert: process.env.DATABUSSSLCERT || fs.readFileSync('./config/certs/client-cert.pem'),
+    //rejectUnauthorized: process.env.DATABUSSSLREJECTUNAUTHORIZED || false,
+    //##### The CA list will be used to determine if server is authorized
+    //ca: process.env.DATABUSSSLCA || fs.readFileSync('./config/certs/ca-cert.pem'),
+    protocol: process.env.DATABUSPROTOCOL || 'mqtt' // mqtt or mqtts
 }
 
 // Logging configuration
 const logger = {
-  level: {
-    auth: process.env.LGAUTHLEVEL || 'debug',
-    db: process.env.LGDBLEVEL || 'debug',
-    routes: process.env.LGROUTESLEVEL || 'debug',
-    app: process.env.LGAPPLEVEL || 'debug',
-    databus: process.env.LGDATABUSLEVEL || 'debug'
-  },
+    level: {
+        auth: process.env.LGAUTHLEVEL || 'debug',
+        db: process.env.LGDBLEVEL || 'debug',
+        routes: process.env.LGROUTESLEVEL || 'debug',
+        app: process.env.LGAPPLEVEL || 'debug',
+        databus: process.env.LGDATABUSLEVEL || 'debug'
+    },
 }
 
 module.exports = {
-  hapiServer,
-  postgres,
-  logger,
-  databus
+    hapiServer,
+    postgres,
+    logger,
+    databus
 };
